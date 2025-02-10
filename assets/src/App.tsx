91bc05a697e5f31e6af827b012311acab8e0e6b5
muqeetsyed <syed@9086>
2025-02-10 23:06:22 +0530

    feat: show products on the UI

--- assets/src/App.tsx
+++ assets/src/App.tsx
@@ -1,35 +1,66 @@
-import {useState, version} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+export const App = () => {
+    return (
+        <>
+            <div className="card">
+                <img src="images/bamboo-cutlery-set.avif" alt="Product Image"/>
+                <div className="card-content">
+                    <h2>Bamboo cutlery set</h2>
+                    <p>Short description of the product</p>
+                    <p className="price">$99.99</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + {version}</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            <div className="card">
+                <img src="images/eco-glass-jar.avif" alt="Product Image"/>
+                <div className="card-content">
+                    <h2>Eco glass jar</h2>
+                    <p>Short description of the product</p>
+                    <p className="price">$43.32</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+
+            <div className="card">
+                <img src="images/eco-glass-storage.avif" alt="Product Image"/>
+                <div className="card-content">
+                    <h2>Eco glass storage</h2>
+                    <p>Short description of the product</p>
+                    <p className="price">$19.43</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+
+            <div className="card">
+                <img src="images/organic-mesh-cloth.avif" alt="Product Image"/>
+                <div className="card-content">
+                    <h2>Organic mesh cloth</h2>
+                    <p>Short description of the product</p>
+                    <p className="price">4.43</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+
+            <div className="card">
+                <img src="images/sponges.avif" alt="Product Image"/>
+                <div className="card-content">
+                    <h2>Sponges</h2>
+                    <p>Short description of the product</p>
+                    <p className="price">$1.43</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+
+            <div className="card">
+                <img src="images/stainless-stell-lunch-box.avif" alt="Product Image"/>
+                <div className="card-content">
+                    <h2>Stainless steel lunch box</h2>
+                    <p>Short description of the product</p>
+                    <p className="price">$100.43</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+        </>
+    );
 }
-
-export default App
