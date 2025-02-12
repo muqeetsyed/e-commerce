d5ee2ba7ed488acb8f79dc4d6e2487c35a7b2723
muqeetsyed <44599346+muqeetsyed@users.noreply.github.com>
2025-02-12 22:33:26 +0530

    use seprate file for product data (#1)
    Co-authored-by: muqeetsyed <syed@9086>

--- assets/src/components/ProductItem.tsx
+++ assets/src/components/ProductItem.tsx
@@ -4,21 +4,20 @@ interface ProductItemProps {
     productImageAlt?: string,
     productDescription?: string,
     productPrice?: string,
+    key?: string,
 }
 
 export const ProductItem = (props: ProductItemProps) => {
-    const {productImage, productImageAlt, productName, productDescription, productPrice} = props;
+    const {key, productImage, productImageAlt, productName, productDescription, productPrice} = props;
     return (
-        <>
-            <div className="card">
-                <img src={productImage} alt={productImageAlt}/>
-                <div className="card-content">
-                    <h2>{productName}</h2>
-                    <p>{productDescription}</p>
-                    <p className="price">${productPrice}</p>
-                    <button>Add to Cart</button>
-                </div>
+        <div className="card" key={key}>
+            <img src={productImage} alt={productImageAlt}/>
+            <div className="card-content">
+                <h2>{productName}</h2>
+                <p>{productDescription}</p>
+                <p className="price">${productPrice}</p>
+                <button>Add to Cart</button>
             </div>
-        </>
+        </div>
     );
 }
--- assets/src/components/ProductItems.tsx
+++ assets/src/components/ProductItems.tsx
@@ -1,57 +1,19 @@
 import {ProductItem} from "./ProductItem.tsx";
+import productItems from "../api/product.json";
 
 export const ProductItems = () => {
     return (
         <>
-            <ProductItem
-                productImage="images/bamboo-cutlery-set.avif"
-                productImageAlt="Product Image"
-                productName="Bamboo cutlery set"
-                productDescription="Short description of the product"
-                productPrice="99.99"
-            />
-
-            <ProductItem
-                productImage="images/eco-glass-jar.avif"
-                productImageAlt="Product Image"
-                productName="Eco glass jar"
-                productDescription="Short description of the product"
-                productPrice="43.32"
-            />
-
-            <ProductItem
-                productImage="images/eco-glass-storage.avif"
-                productImageAlt="Product Image"
-                productName="Eco glass storage"
-                productDescription="Short description of the product"
-                productPrice="12.32"
-            />
-
-
-            <ProductItem
-                productImage="images/organic-mesh-cloth.avif"
-                productImageAlt="Product Image"
-                productName="Organic mesh cloth"
-                productDescription="Short description of the product"
-                productPrice="2.32"
-            />
-
-
-            <ProductItem
-                productImage="images/sponges.avif"
-                productImageAlt="Product Image"
-                productName="Sponges"
-                productDescription="Short description of the product"
-                productPrice="2.32"
-            />
-
-            <ProductItem
-                productImage="images/stainless-stell-lunch-box.avif"
-                productImageAlt="Product Image"
-                productName="Stainless steel lunch box"
-                productDescription="Short description of the product"
-                productPrice="100.2"
-            />
+            {productItems.map((product) => {
+                return <ProductItem
+                    key={product.id}
+                    productImage={product.productImage}
+                    productImageAlt={product.productImageAlt}
+                    productName={product.productName}
+                    productDescription={product.productDescription}
+                    productPrice={product.productPrice}
+                />
+            })}
         </>
     );
 }
